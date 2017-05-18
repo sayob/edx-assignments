2126775f4adfb2c0739eb5c1de6d13f821fe017d
Oluwasayo Babalola <sayob02@gmail.com>
2017-05-18 16:05:45 +0100

    Update assessment2.cs

--- assessment2.cs
+++ assessment2.cs
@@ -8,14 +8,29 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        //with input
+        
         static void Main(string[] args)
         {
-        
-        
-        
-        
-        
+            
+            int row_MaxSize = 8;
+            int col_MaxSize = 8;
+
+          
+            for (int row = 0; row < row_MaxSize; row++)
+            {
+                for (int col = 0; col < col_MaxSize; col++)
+                {
+                    if (((row + col) % 2) == 0)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write("O");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
         
         }
     }
