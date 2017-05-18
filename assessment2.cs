217210da4f3ef5267fbd239e6e5445127840659b
Oluwasayo Babalola <sayob02@gmail.com>
2017-05-18 15:28:45 +0100

    Create assessment2.cs

---
+++ assessment2.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //with input
+        static void Main(string[] args)
+        {
+        
+        
+        
+        
+        
+        
+        }
+    }
+    
+}
